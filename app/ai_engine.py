d2c683cc40f1b63ccdd9c31ed8af9cff36f6d482
Idowu102 <86113735+Idowu102@users.noreply.github.com>
2026-03-12 15:54:36 +0100

    Update ai_engine.py

--- app/ai_engine.py
+++ app/ai_engine.py
@@ -1,46 +1,28 @@
-import ccxt
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
+import random
 
-exchange = ccxt.binance()
 
 def ai_predict(symbol):
 
-    bars = exchange.fetch_ohlcv(symbol, timeframe="1m", limit=60)
+    predictions = [
+        "Bullish breakout possible",
+        "Market likely sideways",
+        "Possible short-term pullback",
+        "Strong upward momentum forming",
+        "Bearish pressure detected"
+    ]
 
-    df = pd.DataFrame(
-        bars,
-        columns=["time","open","high","low","close","volume"]
-    )
-
-    X = np.array(range(len(df))).reshape(-1,1)
-
-    y = df["close"].values
-
-    model = LinearRegression()
-
-    model.fit(X,y)
-
-    future = np.array([[len(df)+1]])
-
-    prediction = model.predict(future)[0]
-
-    return round(float(prediction),2)
+    return random.choice(predictions)
 
 
 def signal_strength(rsi):
 
-    if rsi < 25:
-        return "EXTREME BUY"
-
-    if rsi < 35:
+    if rsi < 30:
         return "STRONG BUY"
-
-    if rsi > 75:
-        return "EXTREME SELL"
-
-    if rsi > 65:
+    elif rsi < 40:
+        return "BUY"
+    elif rsi < 60:
+        return "NEUTRAL"
+    elif rsi < 70:
+        return "SELL"
+    else:
         return "STRONG SELL"
-
-    return "NEUTRAL"
